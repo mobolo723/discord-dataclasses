89adc12eca469bcdc16921c3e256fdbc94434932
mobolo723 <antoinedaniel@icloud.com>
2024-08-11 22:03:17 +0200

    No comment

--- discordDataclasses/models.py
+++ discordDataclasses/models.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, fields
 from typing import Optional, List, Union, Dict, Mapping
 
 from .enums import *
